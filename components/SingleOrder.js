bfccd81745dd6aacb4351062ff8a57c3cb6d050f
zinniLai0217 <fk2946@yahoo.com.tw>
2023-08-14 16:26:43 +0800

    Update SingleOrder.js

--- components/SingleOrder.js
+++ components/SingleOrder.js
@@ -42,8 +42,8 @@ export default function SingleOrder({ line_items, createdAt, ...rest }) {
         </Address>
       </div>
       <div>
-        {line_items.map((item, index) => (
-          <ProductRow key={index}>
+        {line_items.map((item) => (
+          <ProductRow key={item.price_data.product_data.name}>
             <span>{item.quantity} x </span>
             {item.price_data.product_data.name}
           </ProductRow>
