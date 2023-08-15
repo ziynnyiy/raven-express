48f8038ecbaaf4691df3ce85e39d388a2cb06138
Zinni Lai <fk2946@yahoo.com.tw>
2023-08-15 20:09:59 +0800

    Update Logo.js

--- components/icons/Logo.js
+++ components/icons/Logo.js
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
 const StyledImage = styled.img`
-  width: 6px;
-  height: 6px;
+  width: 24px;
+  height: 24px;
 `;
 
 export default function LogoWhite() {
   return (
     <StyledImage
-      src="https://test-for-aws-course-only.s3.ap-southeast-2.amazonaws.com/icons8-bird-48+(2).png"
+      src="https://test-for-aws-course-only.s3.ap-southeast-2.amazonaws.com/icons8-bird-48+(4).pngs"
       alt="logo"
     />
   );
