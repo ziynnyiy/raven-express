5bbee28e31e2a427952afcc199e9d600d16b0a5b
Zinni Lai <fk2946@yahoo.com.tw>
2023-08-20 17:23:59 +0800

    Update NewProducts.js

--- components/NewProducts.js
+++ components/NewProducts.js
@@ -5,7 +5,7 @@ import ProductsGrid from "./ProductsGrid";
 const Title = styled.h2`
   font-size: 2rem;
   margin: 30px 0 20px;
-  font-weight: 500;
+  font-weight: 400;
 `;
 
 const NewProductsWrapper = styled.div`
