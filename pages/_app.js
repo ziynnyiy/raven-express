4650bfdc567ab8dbaca93ea086963f4b493c9ca6
zinniLai0217 <130062212+ziynnyiy@users.noreply.github.com>
2023-11-21 00:54:31 +0800

    fix terminal err
    change way of font-family from import syntax to helmet with link tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 import { CartContextProvider } from "@/components/CartContext";
 import { SessionProvider } from "next-auth/react";
+import { useEffect } from "react";
+import { Helmet } from "react-helmet";
 
 const GlobalStyles = createGlobalStyle`
-
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300;400;500&family=Poppins:wght@300;400;500;600;700;900&display=swap');
 body{
   background-color: #eee;
   padding:0;
@@ -29,8 +29,24 @@ export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
+  // useEffect(() => {
+  //   // After component mount, change the rel attribute to "stylesheet"
+  //   const linkElement = document.querySelector('link[rel="preload"]');
+  //   if (linkElement) {
+  //     linkElement.onload = () => {
+  //       linkElement.rel = "stylesheet";
+  //     };
+  //   }
+  // }, []);
+
   return (
     <>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300;400;500&family=Poppins:wght@300;400;500;600;700;900&display=swap"
+        />
+      </Helmet>
       <GlobalStyles />
       <SessionProvider session={session}>
         <CartContextProvider>
