ca0e249697e327d173e5a09dd9a1bbcdee170422
zinniLai0217 <130062212+ziynnyiy@users.noreply.github.com>
2023-11-21 00:47:27 +0800

    Update SearchIcon.js

--- components/icons/SearchIcon.js
+++ components/icons/SearchIcon.js
@@ -6,7 +6,7 @@ export default function SearchIcon({ className = "w-6 h-6" }) {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className={className}
+      className="w-6 h-6"
     >
       <path
         strokeLinecap="round"
