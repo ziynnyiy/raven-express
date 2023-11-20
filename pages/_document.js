61b9d44cb67ef37f777cdb3c6e401cce5382520c
zinniLai0217 <130062212+ziynnyiy@users.noreply.github.com>
2023-11-21 01:35:21 +0800

    fix FOUC err
    change _document.js  file from tuto version to online example version

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,25 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link
-          rel="icon"
-          type="image/svg+xml"
-          href="https://test-for-aws-course-only.s3.ap-southeast-2.amazonaws.com/icons8-bird-48+(1).png"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
 }
