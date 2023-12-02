ed806243ae343f68f116ffd39eed8d77ec9af9b0
zinniLai0217 <130062212+ziynnyiy@users.noreply.github.com>
2023-12-02 15:09:12 +0800

    modified: spinner

--- components/Spinner.js
+++ components/Spinner.js
@@ -1,4 +1,4 @@
-import { BounceLoader } from "react-spinners";
+import { Stack, CircularProgress } from "@mui/material";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
@@ -17,7 +17,9 @@ const Wrapper = styled.div`
 export default function Spinner({ fullWidth }) {
   return (
     <Wrapper fullWidth={fullWidth}>
-      <BounceLoader speedMultiplier={3} color={"#555"} />
+      <Stack spacing={2} direction="row">
+        <CircularProgress color="primary" />
+      </Stack>
     </Wrapper>
   );
 }
