3aee68f76d3326ae1fddbf1ca2c95971abb70ee2
zinniLai0217 <130062212+ziynnyiy@users.noreply.github.com>
2023-12-04 19:11:22 +0800

    add: facebook icon

---
+++ components/icons/Facebook.js
@@ -0,0 +1,17 @@
+export default function FacebookIcon({ className = "w-6 h-6" }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="99.545"
+      height="99.545"
+      viewBox="0 0 99.545 99.545"
+    >
+      <path
+        id="Facebook-Icon-9imsd"
+        d="M49.766.157A49.772,49.772,0,1,0,99.545,49.926,49.829,49.829,0,0,0,49.766.157M62.152,51.678h-8.1v28.87h-12V51.678H36.348v-10.2h5.706v-6.6c0-4.719,2.243-12.109,12.109-12.109l8.889.037v9.9H56.6a2.441,2.441,0,0,0-2.544,2.778v6H63.2Z"
+        transform="translate(0 -0.157)"
+        fill="#1773ea"
+      />
+    </svg>
+  );
+}
