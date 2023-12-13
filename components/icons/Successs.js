1d5d5615c8366b4547b044af256bf05f23ad1472
zinniLai0217 <130062212+ziynnyiy@users.noreply.github.com>
2023-12-14 00:57:13 +0800

    add: success icon

---
+++ components/icons/Successs.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const StyledSusIcon = styled.img`
+  width: 88px;
+  height: 88px;
+`;
+
+export default function SuccessIcon() {
+  return (
+    <StyledSusIcon src="https://test-for-aws-course-only.s3.ap-southeast-2.amazonaws.com/check+(3).png"></StyledSusIcon>
+  );
+}
